4da47983df6c96dd6408625ed339e25e538af150
medronas <medron@gmx.net>
2021-04-13 02:37:29 +0300

    Add files via upload

--- party.tsx
+++ party.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="party" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="cyberpopinteriortiles/CyberPop Tiles/Mid-TownD.png" width="768" height="768"/>
+ <image source="Mid-TownD.png" width="768" height="768"/>
 </tileset>
